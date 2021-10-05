383fd2c171913a8a7263b5ee9ae8435553abe3c2
Gauransh <gauranshjuneja@gmail.com>
2021-10-05 23:51:26 +0530

    contacts-page-fixed

---
+++ contacts.js
@@ -0,0 +1,24 @@
+let contacts = document.querySelector(".contacts");
+let contributors = "";
+
+fetch(
+  "https://api.github.com/repos/lukes-code/Hacktoberfest-Spooks/contributors"
+)
+  .then((response) => response.json())
+  .then((data) => {
+    data.forEach((user) => {
+      contributors += `<div class="cards">
+      <a href="https://github.com/${user.login}" target="_blank">
+        <img src=${user.avatar_url} alt="${user.login}">
+          <div class="container">
+          <h2>
+            ${user.login}
+          </h2>
+          Contact Now
+          </div> 
+      </a>
+		</div>`;
+    });
+    contacts.innerHTML = contributors;
+  })
+  .catch((err) => console.error(err));
